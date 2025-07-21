6fcf6df829856d87890b4cded8c747127576de27
Bushra Fatima <bushrafatima23@gmail.com>
2025-07-21 15:16:17 +0100

    added provider archive

--- provider.tf
+++ provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.4.0"
     }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.7.1"
+    }
   }
 }
 
@@ -11,11 +15,3 @@ provider "aws" {
   region = "us-east-2"
 }
 
-terraform {
-  required_providers {
-    archive = {
-      source = "hashicorp/archive"
-      version = "2.7.1"
-    }
-  }
-}
